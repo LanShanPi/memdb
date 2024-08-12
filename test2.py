04b586bb4e8fe0476c6276fe7e7ec3489cbd1650
LanShanPi <2206851664@qq.com>
2024-08-12 10:02:38 +0800

    update code

--- test2.py
+++ test2.py
@@ -46,6 +46,7 @@ import requests
 
 
 # 查询重要信息
+
 url = "http://42.123.114.119:8598/similar_search_inf/"
 headers = {"Content-Type": "application/json"}
 data = {
@@ -56,5 +57,7 @@ data = {
 response = requests.post(url, headers=headers, json=data)
 # 如果需要查看响应
 status = response.status_code
-result = response.json()  # 如果返回的数据是JSON格式
+retrieval_result = response.json()  # 如果返回的数据是JSON格式
 # result 格式为：{'data': [['今天是我生日，我好开心', '1.6624228']]}
+prompt_plus = f"注意！'data_add'是根据用户的话从数据库中检索到的数据，请先根据上下文判断这些数据对回复用户的话是否有用，若有用则根据这些数据进行回复，若无用则自己根据用户的话进行回复。data_add：{retrieval_result['data']}."
+print(prompt_plus)
