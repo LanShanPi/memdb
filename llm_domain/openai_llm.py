72e4b96e9a531d7125b228c6a0241c608c7be87f
LanShanPi <2206851664@qq.com>
2024-08-23 12:35:12 +0800

    update code

--- llm_domain/openai_llm.py
+++ llm_domain/openai_llm.py
@@ -8,6 +8,7 @@ client = OpenAI(
     base_url=Base_Url,
     api_key=Api_Key
 )
+
 def get_openai_response(query,system,model="gpt-4o-mini"):
     # model="gpt-3.5-turbo",
     # query 类型为：str
@@ -18,6 +19,10 @@ def get_openai_response(query,system,model="gpt-4o-mini"):
     )
     # 返回类型为str："{'information':'无重要信息'}"
     return response.choices[0].message.content
+
+query = "'user_history':['我姓洪'],'user_new':'叫志理'"
+system = "请根据user_history中的所有内容，对user_new的内容进行补全，要以第三人称描述的语气总结，如：用户XXX"
+print(get_openai_response(query,system))
 
 
 
--- redis_domain/redis_processer.py
+++ redis_domain/redis_processer.py
@@ -3,8 +3,6 @@ import redis
 # 连接 Redis 服务器
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-# 用户ID
-user_id = 'user1'
 
 # 存储用户对话
 def store_conversation(user_id, conversation):
@@ -13,26 +11,22 @@ def store_conversation(user_id, conversation):
 
 # 获取用户的固定数量的对话（从末尾开始）
 def get_conversations(user_id, num_conversations):
+    # # num_conversations=3，取出最近的3条对话
     # 获取列表的长度
     list_length = r.llen(f"conversations:{user_id}")
     # 计算要提取的起始索引
     start = max(0, list_length - num_conversations)
     end = list_length - 1
     # 取出用户ID对应的对话（从末尾开始，按顺序,start=0,end=-1,就是取全部的数据）
-    return r.lrange(f"conversations:{user_id}", start, end)
+    datas = r.lrange(f"conversations:{user_id}", start, end)
+    result = []
+    for data in datas: 
+        result.append(data.decode('utf-8'))
+    return result
 
-# 示例：存储用户对话
-store_conversation(user_id, "你好啊")
-store_conversation(user_id, "我姓洪")
-store_conversation(user_id, "叫志理")
-store_conversation(user_id, "我家河南的")
 
-# 示例：获取用户最近的对话
-num_conversations = 4  # 例如，取出最近的3条对话
-conversations = get_conversations(user_id, num_conversations)
-
-dialog = []
-for conv in conversations:
-    dialog.append(conv.decode('utf-8'))
-
-print(dialog)
+# # 示例：存储用户对话
+# store_conversation(user_id, "你好啊")
+# # 示例：获取用户最近的对话
+# num_conversations = 4  # 例如，取出最近的3条对话
+# result = get_conversations(user_id, num_conversations)
